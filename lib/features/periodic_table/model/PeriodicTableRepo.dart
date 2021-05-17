44d8a71c11a83db85c0dfd89cd84119aca0f26c6
mayada khaled <47379816+mayadakhaled66@users.noreply.github.com>
2021-05-17 20:03:29 +0200

    most of app logic

--- lib/features/periodic_table/model/PeriodicTableRepo.dart
+++ lib/features/periodic_table/model/PeriodicTableRepo.dart
@@ -7,14 +7,7 @@ class PeriodicTableRepo {
   Future<PeriodicTableModel> getUsers() async {
     final url = "https://reqres.in/api/users";
     try {
-      final response = await http.get(
-        url,
-        // headers: {"app-id" : "602ac221ac5aaa7e77047aa2"},
-        // body: {
-        //   "email": username,
-        //   "password": password
-        // },
-      );
+      final response = await http.get(url);
 
       if (response.statusCode == 201||response.statusCode == 200) {
         return PeriodicTableModel.fromJson(jsonDecode(response.body));
@@ -25,64 +18,4 @@ class PeriodicTableRepo {
       rethrow;
     }
   }
-  // Future<TagModel> getTags() async {
-  //   final url = "https://dummyapi.io/data/api/tag";
-  //   try {
-  //     final response = await http.get(
-  //       url,headers: {"app-id" : "602ac221ac5aaa7e77047aa2"},
-  //       // body: {
-  //       //   "email": username,
-  //       //   "password": password
-  //       // },
-  //     );
-  //
-  //     if (response.statusCode == 201||response.statusCode == 200) {
-  //       return TagModel.fromJson(jsonDecode(response.body));
-  //     } else {
-  //       throw Exception('Failed to with response code state ${response.statusCode}');
-  //     }
-  //   } catch (error) {
-  //     rethrow;
-  //   }
-  // }
-  // Future<PostModel> getPosts() async {
-  //   final url = "https://dummyapi.io/data/api//post";
-  //   try {
-  //     final response = await http.get(
-  //       url,headers: {"app-id" : "602ac221ac5aaa7e77047aa2"},
-  //       // body: {
-  //       //   "email": username,
-  //       //   "password": password
-  //       // },
-  //     );
-  //
-  //     if (response.statusCode == 201||response.statusCode == 200) {
-  //       return PostModel.fromJson(jsonDecode(response.body));
-  //     } else {
-  //       throw Exception('Failed to with response code state ${response.statusCode}');
-  //     }
-  //   } catch (error) {
-  //     rethrow;
-  //   }
-  // }
-  // Future<PostModel> getPostsPerUserId({@required String userId}) async {
-  //   final url = "https://dummyapi.io/data/api/user/$userId/post";
-  //   try {
-  //     final response = await http.get(
-  //       url,headers: {"app-id" : "602ac221ac5aaa7e77047aa2"},
-  //       // body: {
-  //       //   "email": username,
-  //       //   "password": password
-  //       // },
-  //     );
-  //
-  //     if (response.statusCode == 201||response.statusCode == 200) {
-  //       return PostModel.fromJson(jsonDecode(response.body));
-  //     } else {
-  //       throw Exception('Failed to with response code state ${response.statusCode}');
-  //     }
-  //   } catch (error) {
-  //     rethrow;
-  //   }
-  // }
 }
